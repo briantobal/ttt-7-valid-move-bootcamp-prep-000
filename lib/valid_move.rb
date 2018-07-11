ba0c70a77110f3dd0d02e82a14a13df1d8449116
Brian Tobal <brian.tobal@gmail.com>
2018-07-11 12:16:23 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,10 +2,6 @@ def position_empty?(board, index)
   board[index] == ' '
 end
 
-def position_taken?(board, index)
-  !position_empty?(board, index)
-end
-
 def valid_move?(board, index)
   index.between?(0,8) && position_empty?(board, index)
 end
